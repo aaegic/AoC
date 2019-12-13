5c24e72476ec533c68bf1cdce6ada52e6e191810
Adrian Maliszewski <adrian@aftre.net>
2019-12-13 14:55:54 -0500

    2019 updates

---
+++ 2019/Day6/Part1/orbit.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+from var_dump import var_dump
+
+fh = open("input.txt", mode='r')
+input_list = fh.read()
+fh.close()
+
+input_list = input_list.strip()
+input_list = input_list.split("\n")
+
+def findp (name, dict):
+    for i, j in dict.items():
+        for k in j:
+            if k == name:
+                return i
+
+start = ""
+orbit = {}
+pdist = {}
+pendn = []
+paths = []
+
+for i in input_list:
+    _a, _b = i.split(")")
+    if _a in orbit:
+        orbit[_a].append(str(_b))
+    else:
+        orbit[_a] = []
+        orbit[_a].append(str(_b))
+
+    if not start:
+        start = _a
+        pdist[_a] = 0
+
+for i, j in orbit.items():
+    for k in orbit.get(i):
+        if not k in orbit:
+            pendn.append(k)
+
+for i in pendn:
+    p = []
+    p.append(i)
+    while not i is start:
+        i = findp(i, orbit)
+        p.append(i)
+    paths.append(list(reversed(p)))
+
+for i in paths:
+    for j in range(len(i)):
+        pdist[i[j]] = j
+
+count = 0
+for i, j in pdist.items():
+    count = count + j
+
+#var_dump(pdist)
+print(count)
+#print(orbit)
+
+# {'COM': ['B'], 'B': ['C', 'G'], 'C': ['D'], 'D': ['E', 'I'], 'E': ['F', 'J'], 'G': ['H'], 'J': ['K'], 'K': ['L']}
+# ict_items([('COM', ['B']), ('B', ['C', 'G']), ('C', ['D']), ('D', ['E', 'I']), ('E', ['F', 'J']), ('G', ['H']), ('J', ['K']), ('K', ['L'])])
