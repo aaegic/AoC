5288dfceb1f23f6951d322a6a2dca3bf2a7431a8
Adrian Maliszewski <adrian@aftre.net>
2020-12-16 00:11:49 -0500

    2020 updates

--- 2020/Day13/Part1/Day13a.py
+++ 2020/Day13/Part1/Day13a.py
@@ -2,7 +2,13 @@
 
 import math
 
-print(math.prod(sorted(list(zip(list(map(lambda x: (math.ceil((int(str(open("input.txt", mode='r').read()).strip().split("\n")[0]) / x)) * x) - int(str(open("input.txt", mode='r').read()).strip().split("\n")[0]), list(map(int, list(filter(lambda x: x != 'x', str(open("input.txt", mode='r').read()).strip().split("\n")[1].split(","))))))),list(map(int, list(filter(lambda x: x != 'x', str(open("input.txt", mode='r').read()).strip().split("\n")[1].split(",")))))))).pop(0)))
+print(math.prod(sorted(list(zip(list(map(lambda x: (math.ceil((int(str(open(   \
+    "input.txt", mode='r').read()).strip().split("\n")[0]) / x)) * x) - int(   \
+    str(open("input.txt", mode='r').read()).strip().split("\n")[0]), list(map( \
+    int, list(filter(lambda x: x != 'x', str(open("input.txt", mode='r').read()\
+    ).strip().split("\n")[1].split(","))))))),list(map(int, list(filter(lambda \
+    x: x != 'x', str(open("input.txt", mode='r').read()).strip().split("\n")   \
+    [1].split(",")))))))).pop(0)))
 
 """
    ___       __  __               _       ___
