6a0cd168731f31c3939da7e7ebb3625c2eda39c9
Adrian Maliszewski <adrian@aftre.net>
2020-12-26 17:59:39 -0500

    2020 updates

--- 2020/Day18/Part1/Day18a.py
+++ 2020/Day18/Part1/Day18a.py
@@ -2,23 +2,32 @@
 
 import itertools
 
-fh = open("input-test.txt")
+fh = open("input.txt")
 eq = fh.read()
 fh.close()
-
-#eq = "2 * 3 + (4 * 5)".replace(' ', '') # 26
-#eq = "5 + (8 * 3 + 9 + 3 * 4 * 3)".replace(' ', '') # 437
-#eq = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".replace(' ', '') # 12240
-#eq = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".replace(' ', '') # 13632
-#eq = "(8 + (7 + 9)) * 6".replace(' ', '')
 
 def rindex(li:list, ch:str, lrange:int, rrange:int) -> int:
     for i in range(rrange, lrange - 1, -1):
         if li[i] == ch:
             return i
 
-def compute():
-
+def compute(input:list) -> int:
+    lft, opr = None, None
+    
+    for e in input:
+        if opr is not None:
+            if opr == '+':
+                lft = lft + e
+            elif opr == '*':
+                lft = lft * e
+            
+            opr = None
+        elif lft is not None:
+            opr = e
+        else:
+            lft = e
+            
+    return lft
 
 eqlst = list()
 reslt = list()
@@ -35,50 +44,14 @@ for line in eq.split("\n"):
     
     eqlst.append(eq)
 
-print(eqlst)
+for eq in eqlst:
+    while '(' in eq:
+        rpi = eq.index(')')
+        lpi = rindex(eq, '(', 0, rpi)
 
-eq = eqlst[3]
-
-while '(' in eq:
-    lft, opr = None, None
+        eq = list(itertools.chain(eq[:lpi], [compute(eq[lpi+1:rpi])], eq[rpi+1:]))
     
-    rpi = eq.index(')')
-    lpi = rindex(eq, '(', 0, rpi)
+    reslt.append(compute(eq))
 
-    print(eq)
-    print(rpi, eq[rpi], lpi, eq[lpi])
-
-    for e in eq[lpi+1:rpi]:
-        if opr is not None:
-            if opr == '+':
-                lft = lft + e
-            elif opr == '*':
-                lft = lft * e
-            
-            opr = None
-        elif lft is not None:
-            opr = e
-        else:
-            lft = e
-
-    eq = list(itertools.chain(eq[:lpi], [lft], eq[rpi+1:]))
-    lft, opr = None, None
-
-print(eq)
-
-for e in eq:
-    if opr is not None:
-        if opr == '+':
-            lft = lft + e
-        elif opr == '*':
-            lft = lft * e
-        
-        opr = None
-    elif lft is not None:
-        opr = e
-    else:
-        lft = e
-
-eq = lft
-
-print(eq)
+print(reslt)
+print(sum(reslt))
