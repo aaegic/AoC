b3d719ecaf1108cee5deac8b972d6c16c0f5d2a2
Adrian Maliszewski <adrian@agile.dmz.aftre.net>
2021-12-07 01:43:16 -0500

    2021 Day 7 - Using Gauss Summation

--- 2021/Day07/Part2/Day07b.py
+++ 2021/Day07/Part2/Day07b.py
@@ -3,7 +3,9 @@
 itxt = open("input", mode='r').read().strip().splitlines()
 crab = list(map(int, itxt[0].split(',')))
 
-fuel = min([sum([sum(range(1,abs(i-c)+1)) for c in crab]) for i in range(max(crab)*2)])
+#fuel = min([sum([sum(range(1,abs(i-c)+1)) for c in crab]) for i in range(max(crab)*2)])
+#using Guass Summation
+fuel = min([sum([((abs(i-c))/2)*((abs(i-c))+1) for c in crab]) for i in range(max(crab)*2)])
 
-print(fuel)
+print(int(fuel))
 
