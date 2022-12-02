8791c3f7c0f4ea9dd027f213acb15dbf924a654c
Adrian Maliszewski <adrian@aftre.net>
2022-12-01 23:45:11 -0500

    2022 Advent of Code!

---
+++ 2022/Day01/Part2/Day01b.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from var_dump import var_dump
+from icecream import ic
+import sys
+
+def main () -> int:
+
+    itxt = open("input", mode='r').read().split('\n\n')
+
+    itxt = [i.strip().split('\n') for i in itxt]
+
+    itxt = sorted([sum(list(map(int, i))) for i in itxt])
+    
+    itxt.reverse()
+    
+    ic(sum(itxt[0:3]))
+    
+
+if __name__ == '__main__':
+    sys.exit(main()) 
+    
+    
