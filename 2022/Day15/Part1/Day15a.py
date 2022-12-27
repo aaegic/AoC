a4d44517a7fe9975c8026d6d233f773de4e24008
Adrian Maliszewski <adrian@aftre.net>
2022-12-27 02:46:02 -0500

    2022 Advent of Code!

--- 2022/Day15/Part1/Day15a.py
+++ 2022/Day15/Part1/Day15a.py
@@ -7,9 +7,9 @@ import re
 
 def main () -> None:
 
-    itxt = open("input-test", mode='r').readlines()
+    itxt = open("input", mode='r').readlines()
     itxt = [re.split('=|:|,', i.strip()) for i in itxt]
-    itxt = [((int(i[1]),int(i[3])),(int(i[5]),int(i[7]))) for i in itxt]
+    mmap = [((int(i[1]),int(i[3])),(int(i[5]),int(i[7]))) for i in itxt]
     #itxt = [(np.array((int(i[1]),int(i[3]))),np.array((int(i[5]),int(i[7])))) for i in itxt]
     #sr = [(s[0], abs((s[0] - s[1])[0]) + abs((s[0] - s[1])[1])) for s in itxt]
     #xx = set()
@@ -17,27 +17,54 @@ def main () -> None:
     #ic(xx)
     #return
     
-    #ss, bs = [i[0] for i in itxt], [i[1] for i in itxt]
+    sensors, beacons = [i[0] for i in mmap], [i[1] for i in mmap]
     
-    
+    ic(mmap)
+    ic(sensors + beacons)
     smap = set()
     
-    for sensor in itxt:
-        d = np.array(sensor[0]) - np.array(sensor[1])
-        r = abs(d[0]) + abs(d[1])
+    YTARGET = 10
+    YTARGET = 2000000
+    
+    for sensor, beacon in mmap:
+        rad = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+        ic(sensor, rad)
 
-        for x in range(r+1): 
-            for y in range(0,(r+1)-x):
-                smap.update({tuple(np.array(sensor[0]) + coord) \
-                    for coord in {(x, y),(-x, y),(x, -y),(-x, -y)}})
-                
-                #for c in {(x, y),(-x, y),(x, -y),(-x, -y)}:
-                #    smap.add(tuple(np.array(s[0]) + c))
+        if sensor[1] < YTARGET:
+            yreach = (np.array(sensor) + np.array((0, rad)))[1]
+            if yreach < YTARGET: continue
+        elif sensor[1] > YTARGET:
+            yreach = (np.array(sensor) - np.array((0, rad)))[1]
+            if yreach > YTARGET: continue
+        else:
+            pass
+
+        for x in range(abs(yreach - YTARGET) + 1):
+            if (sensor[0] + x, YTARGET) not in sensors + beacons:
+                smap.add((sensor[0] + x, YTARGET))
+            if (sensor[0] - x, YTARGET) not in sensors + beacons:
+                smap.add((sensor[0] - x, YTARGET))
             
+            #ic(sensor[0] + x, YTARGET)
+            #ic(sensor[0] - x, YTARGET)
+
+        if 0:
+            for x in range(rad+1): 
+                for y in range(0,(rad+1)-x):
+                    #smap.update({tuple(np.array(sensor[0]) + coord) \
+                    #    for coord in {(x, y),(-x, y),(x, -y),(-x, -y) }})
+                    
+                    for coord in {(x, y),(-x, y),(x, -y),(-x, -y)}:
+                        if sensor[0][1] + coord[1] == YTARGET: 
+                            ic((sensor[0][0] + coord[0], sensor[0][1] + coord[1]))
+                            smap.add((sensor[0][0] + coord[0], sensor[0][1] + coord[1]))
     #ic(smap)
 
-    x = [c for c in smap if c[1] == 10 and c not in [i[0] for i in itxt] and c not in [i[1] for i in itxt]]
-    ic(x, len(x))
+    ic()
+    #x = [c for c in smap if c[1] == YTARGET and c not in [i[0] for i in mmap] and c not in [i[1] for i in mmap]]
+    ic()
+    #ic(len(x))
+    ic(len(smap))
 
 
 if __name__ == '__main__':
