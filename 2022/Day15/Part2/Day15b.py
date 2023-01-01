fde16664e68560b81d433ab3c9587c3b624c6b59
Adrian Maliszewski <adrian@aftre.net>
2023-01-01 18:30:51 -0500

    2022 Advent of Code!

--- 2022/Day15/Part2/Day15b.py
+++ 2022/Day15/Part2/Day15b.py
@@ -2,83 +2,121 @@
 
 import sys
 import numpy as np
+from numpy.linalg import norm 
+import matplotlib.pyplot as plt
+import math 
 from icecream import ic
 import re
 
 def main () -> None:
 
+    #https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
+    
+    def line_intersection(line1, line2):
+        xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+        def det(a, b):
+            return a[0] * b[1] - a[1] * b[0]
+
+        div = det(xdiff, ydiff)
+        if div == 0:
+            raise Exception('lines do not intersect')
+
+        d = (det(*line1), det(*line2))
+        x = det(d, xdiff) / div
+        y = det(d, ydiff) / div
+        return x, y
+
+    print line_intersection((A, B), (C, D))
+    
+    
     itxt = open("input-test", mode='r').readlines()
     itxt = [re.split('=|:|,', i.strip()) for i in itxt]
     itxt = [((int(i[1]),int(i[3])),(int(i[5]),int(i[7]))) for i in itxt]
 
     sensors, beacons = [i[0] for i in itxt], [i[1] for i in itxt]
 
-    RANGE = 20
-    #RANGE = 4000000
-    
     smap = set()
-    mrow = set()
+    edges_p = set()
+    edges_n = set()
     
-    for y in [10]:#range(RANGE + 1):
-        mrow = {(i[0], i[0]) for i in sensors + beacons if i[1] == y}
-        ic(y)
-        for sensor, beacon in zip(sensors, beacons):
-            rad = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+    for sensor, beacon in zip(sensors, beacons):
+        rad = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+        rad = norm(np.array(np.array(sensor) - np.array(beacon)), 1)
+        
+        ic(rad)
+        xs = []
+        ys = []
 
-            if sensor[1] < y:
-                yreach = (np.array(sensor) + np.array((0, rad)))[1]
-                if yreach < y: continue
-                xreach = yreach - y
-            elif sensor[1] > y:
-                yreach = (np.array(sensor) - np.array((0, rad)))[1]
-                if yreach > y: continue
-                xreach = yreach - y
-            else:
-                xreach = rad
+        edges_p.update({
+            ((sensor[0] + rad, sensor[1]),(sensor[0], sensor[1] + rad)),    # / bottom right
+            ((sensor[0] - rad, sensor[1]),(sensor[0], sensor[1] - rad)) })  # / top left
+        
+        xs.append(sensor[0] + rad)
+        ys.append(sensor[1])
+        xs.append(sensor[0])
+        ys.append(sensor[1] + rad)
+        
+        plt.plot(xs, ys, linestyle = 'dotted')
+        xs = []
+        ys = []
+        
+        xs.append(sensor[0] - rad)
+        ys.append(sensor[1])
+        xs.append(sensor[0])
+        ys.append(sensor[1] - rad)
+        
+        plt.plot(xs, ys, linestyle = 'dotted')
+        
+        
+        edges_n.update({
+            ((sensor[0], sensor[1] + rad),(sensor[0] - rad, sensor[1])),    # \ bottom left
+            ((sensor[0], sensor[1] - rad),(sensor[0] + rad, sensor[1])) })  # \ top right
 
-            mrow.add(tuple(sorted((-(sensor[0] + xreach), sensor[0] + xreach))))
+        xs = []
+        ys = []
 
-            if 0:
-                for x in range(abs(xreach) + 1):
-                    if sensor[0] + x not in mrow:
-                        #if 0 < sensor[0] + x <= RANGE:
-                        mrow.add(sensor[0] + x)
-                        ic(sensor[0] + x)
-                    if sensor[0] - x not in mrow:
-                        #if 0 < sensor[0] - x <= RANGE:
-                        mrow.add(sensor[0] - x)
-                        ic(sensor[0] - x)
+        xs.append(sensor[0])
+        ys.append(sensor[1] + rad)
+        xs.append(sensor[0] - rad)
+        ys.append(sensor[1])
         
-        ic(y, mrow)
+        plt.plot(xs, ys, linestyle = 'dotted')
+        xs = []
+        ys = []
         
-        mrow = sorted(mrow)
-        ic(mrow)
-        
-        frow = [list(mrow[0])]
-        ic(frow)
-        
-        for i in range(1,len(mrow)):
-            if frow[-1][1] <= mrow[i][1]:
-                ic()
-                frow[-1][1] = mrow[i][1]
-            elif mrow[i] not in frow:
-                ic()
-                frow.append(list(mrow[i]))
-            
-        ic(frow)
-                
-            
-            
-            
-        
-        
-        
-        
-        
-        #for x in range(RANGE + 1):
-        #    if x not in mrow:
-        #        ic((x,y))
-        #        smap.add((x, y))
+        xs.append(sensor[0])
+        ys.append(sensor[1] - rad)
+        xs.append(sensor[0] + rad)
+        ys.append(sensor[1])
+
+        plt.plot(xs, ys, linestyle = 'dotted')
+
+
+    ic(edges_n)
+    ic(edges_p)
+    plt.show()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return
+
+
             
     ic(smap)
     ic(len(smap))
